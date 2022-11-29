ca934dc80cce9c4b310f76b3b92422b238cf1e54
khanhnd <khanh.nttu0204@gmail.com>
2022-11-29 21:40:48 +0700

    add-react-query

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import React, { Fragment } from 'react'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import DefaultComponent from './components/DefaultComponent/DefaultComponent'
 import { routes } from './routes'
 
 function App() {
 
+
+  // useEffect(() => {
+  //   fetchApi()
+  // }, [])
+  const fetchApi = async () => {
+    const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all`)
+    return res.data
+  }
+
+  const query = useQuery({ queryKey: ['todos'], queryFn: fetchApi })
+
+  console.log('query', query)
+
   return (
     <div>
-        <Router>
-          <Routes>
-            {routes.map((route) => {
-              const Page = route.page
-              const Layout = route.isShowHeader ?  DefaultComponent : Fragment
-              return (
-                <Route key={route.path} path={route.path} element={
-                  <Layout>
-                    <Page />
-                  </Layout>
-                } />
-              )
-            })}
-          </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+            const Page = route.page
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment
+            return (
+              <Route key={route.path} path={route.path} element={
+                <Layout>
+                  <Page />
+                </Layout>
+              } />
+            )
+          })}
+        </Routes>
+      </Router>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,17 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux'
-import { store } from './redux/store' 
+import { store } from './redux/store'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 // import "~slick-carousel/slick/slick.css"; 
 // import "~slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient()
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
