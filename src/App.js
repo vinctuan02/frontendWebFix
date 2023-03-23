986a848f2cf258a7db4aad1b81963c80acba3b95
khanhnd <khanh.nttu0204@gmail.com>
2023-03-23 14:53:46 +0700

    fix-bug-auth

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
           <Routes>
             {routes.map((route) => {
               const Page = route.page
-              const ischeckAuth = !route.isPrivate || user.isAdmin
+              const ischeckAuth = !route.isPrivated || user.isAdmin || ''
               const Layout = route.isShowHeader ? DefaultComponent : Fragment
               return (
                 <Route key={route.path} path={ischeckAuth && route.path} element={
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { PersistGate } from 'redux-persist/integration/react'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient()
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
@@ -23,7 +23,7 @@ root.render(
       </Provider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
